0312eb108ef536c75feeb4dbd2d20914d18c41f8
Adam Brodin <worecraft@gmail.com>
2021-05-16 21:48:53 +0200

    Fixed GUI lining up with backend

--- src/com/adambrodin/osrsflipper/core/Main.java
+++ src/com/adambrodin/osrsflipper/core/Main.java
@@ -52,8 +52,9 @@ public class Main extends AbstractScript {
             Flipper.ExecuteFlips();
         } else if (hasLoggedIn) {
             getRandomManager().disableSolver(RandomEvent.LOGIN);
-            loggingBackInMillis = System.currentTimeMillis() + (((BotConfig.LOGOUT_SLEEP_DURATION_MINUTES + Calculations.random(-3, 3)) * 60) * 1000);
-            log("Logged out! Waiting " + BotConfig.LOGOUT_SLEEP_DURATION_MINUTES + " minutes before logging back in.");
+            int logoutTimeMinutes = BotConfig.LOGOUT_SLEEP_DURATION_MINUTES + Calculations.random(-3, 3);
+            loggingBackInMillis = System.currentTimeMillis() + ((logoutTimeMinutes * 60) * 1000);
+            log("Logged out! Waiting " + logoutTimeMinutes + " minutes before logging back in.");
             sleepUntil(() -> Client.getGameState() == GameState.LOGGED_IN, (BotConfig.LOGOUT_SLEEP_DURATION_MINUTES * 60) * 1000);
             log("Logging back in!");
             getRandomManager().enableSolver(RandomEvent.LOGIN);
