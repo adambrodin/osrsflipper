7bf5708ca043938ffce5e97157e9e148edb6647a
Adam Brodin <adambrodindeveloper@gmail.com>
2021-06-04 00:52:45 +0200

    Config tweaks

--- src/com/adambrodin/osrsflipper/misc/BotConfig.java
+++ src/com/adambrodin/osrsflipper/misc/BotConfig.java
@@ -17,22 +17,22 @@ public class BotConfig {
     // FLIP CONFIGURATIONS
     public static final boolean ONLY_F2P_ITEMS = false;
     public static final int ITEM_VOLUME_GREAT = 100000; // Determine what min volume that gets extra score when considered
-    public static final float MAX_CASHSTACK_PERCENTAGE_PER_FLIP = 0.7f; // The max percentage of the current money to use in a single flip (values less than 100 provide a broader range of items instead of spending everything on one item)
+    public static final float MAX_CASHSTACK_PERCENTAGE_PER_FLIP = 0.6f; // The max percentage of the current money to use in a single flip (values less than 100 provide a broader range of items instead of spending everything on one item)
     public static final float MIN_PROFIT_FOR_FLIP = 5000;
-    public static final boolean INCLUDE_RISKY_FLIP = false;
+    public static final boolean INCLUDE_RISKY_FLIP = true;
     public static final float MAX_NO_RESTRICTIONS_MARGIN_PERC = 35;
-    public static final float MAX_NO_RESTRICTIONS_CASHSTACK_PERC = 0.3f; // The max amount of % money to use for the "risky" flip (without requirements)
+    public static final float MAX_NO_RESTRICTIONS_CASHSTACK_PERC = 0.2f; // The max amount of % money to use for the "risky" flip (without requirements)
     public static final int MIN_GOLD_FOR_FLIP = 50000; // Minimum amount of gp in inventory to start new flips
-    public static final int MIN_CASHSTACK_FOR_PERCENTAGE_FLIP = 1000000; // Min amount of gp to only use MAX_CASHSTACK_PERCENTAGE_PER_FLIP instead of 100%
+    public static final int MIN_CASHSTACK_FOR_PERCENTAGE_FLIP = 2000000; // Min amount of gp to only use MAX_CASHSTACK_PERCENTAGE_PER_FLIP instead of 100%
     public static final int MAX_FLIP_ACTIVE_TIME_MINUTES = 50;
     public static final int MIN_FLIP_ITEMS_FORCE_SELL = 20; // The minimum amount of items to force-sell if not all items were bought
-    public static final float MAX_FLIP_COMPLETED_PERC_EXIT = 95; // The maximum amount of % completed before force-exiting it
+    public static final float MAX_FLIP_COMPLETED_PERC_EXIT = 90; // The maximum amount of % completed before force-exiting it
     public static final float MIN_FLIP_NORMAL_SELL_PERC = 25f; // The minimum amount of % sell to sell it normally (otherwise force-sell)
     public static final float MAX_ITEM_MARGIN_PERCENTAGE = 20; // Max percentage margin for an item to even be considered at all
     public static final float MAX_VALID_MARGIN_PERCENTAGE = 10; // Maximum percentage for an item to considered normally (this is to minimize items such as runes with 25% margin)
     public static final float MIN_ITEM_MARGIN_PERCENTAGE = 0.5f; // Min margin for an item to be considered
     public static final float MIN_ITEM_MARGIN_GP = 2;
-    public static final float MIN_ITEM_VOLUME = 20000; // Min volume for an item in the endpoint timespan
+    public static final float MIN_ITEM_VOLUME = 40000; // Min volume for an item in the endpoint timespan
     public static final int MIN_ITEM_PRICE_FOR_CUT = 100; // Min item price for it to be cut (to prevent cutting very cheap items)
 
     public static final List<String> BLOCKED_ITEMS = Arrays.asList("Swamp tar", "Swamp paste", "Thread", "Bucket of water", "Bucket", "Jug", "Jug of water", "Fishing bait", "Trading sticks", "Old school bond");
